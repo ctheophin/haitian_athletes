2a908edb513143f6027653a7348fd8f6c6c9ba1c
ctheophin <43359419+ctheophin@users.noreply.github.com>
2018-12-10 20:00:33 -0500

    Finished Project

---
+++ main.js
@@ -0,0 +1,86 @@
+$(document).ready(() => {
+  $('.nba').hide();
+  $('.nfl').hide();
+  $('.mlb').hide();
+  $('.others').hide();
+//NBA
+$('#biobut').on('click', () => {
+  $('#blakebio').toggle();
+});
+$('#skalbut').on('click', () => {
+  $('#skalbio').toggle();
+})
+$('#noelbut').on('click', () => {
+  $('#noelbio').toggle();
+})
+//NFL
+$('#alexbut').on('click', () => {
+  $('#alexbio').toggle();
+});
+$('#desirbut').on('click', () => {
+  $('#desirbio').toggle();
+});
+$('#rickybut').on('click', () => {
+  $('#rickybio').toggle();
+});
+$('#johnbut').on('click', () => {
+  $('#johnbio').toggle();
+});
+$('#liugetbut').on('click', () => {
+  $('#liugetbio').toggle();
+});
+$('#sonybut').on('click', () => {
+  $('#sonybio').toggle();
+});
+$('#giobut').on('click', () => {
+  $('#giobio').toggle();
+});
+$('#vladbut').on('click', () => {
+  $('#vladbio').toggle();
+});
+$('#garconbut').on('click', () => {
+  $('#garconbio').toggle();
+});
+$('#karlbut').on('click', () => {
+  $('#karlbio').toggle();
+});
+$('#kplbut').on('click', () => {
+  $('#kplbio').toggle();
+});
+$('#jppbut').on('click', () => {
+  $('#jppbio').toggle();
+});
+$('#marcellbut').on('click', () => {
+  $('#marcellbio').toggle();
+});
+$('#easleybut').on('click', () => {
+  $('#easleybio').toggle();
+});
+$('#gordonbut').on('click', () => {
+  $('#gordonbio').toggle();
+});
+$('#lamurbut').on('click', () => {
+  $('#lamurbio').toggle();
+});
+$('#whitbut').on('click', () => {
+  $('#whitbio').toggle();
+});
+//MLB
+$('#toukibut').on('click', () => {
+  $('#toukibio').toggle();
+});
+
+//Others
+$('#naomibut').on('click', () => {
+  $('#nabio').toggle();
+});
+$('#andrebut').on('click', () => {
+  $('#andrebio').toggle();
+});
+$('#jozybut').on('click', () => {
+  $('#jozybio').toggle();
+});
+
+
+
+});
